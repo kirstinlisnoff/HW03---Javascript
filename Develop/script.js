95dd903f3fd02e4caf12df038ead145e6ac0a976
Kiki <kirstinlisnoff5@gmail.com>
2025-01-13 15:07:54 -0500

    added get random employee fn

--- Develop/script.js
+++ Develop/script.js
@@ -35,7 +35,12 @@ const displayAverageSalary = function (employeesArray) {
 // Select a random employee
 const getRandomEmployee = function (employeesArray) {
   // TODO: Select and display a random employee
+  const pickEmployee = Math.floor(Math.random() * employeesArray.length);
+  const randomEmployee = employeesArray[pickEmployee];
+  console.log(`Congratulations to ${randomEmployee.firstName} ${randomEmployee.lastName}, our random drawing winner!`);
 };
+
+
 
 /*
   ====================
